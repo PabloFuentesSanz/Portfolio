1f8b4c031ca7c36064a9827fe87b125b7a7bf3b5
Pablo <51823158+PabloFuentesSanz@users.noreply.github.com>
2024-09-18 13:21:37 +0200

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -28,7 +28,7 @@ function Header() {
           className="linkContact"
         >
           <img
-            src="https://telecos.upc.edu/es/shared/images/social/logos-rodons/linkedin-circle.png/@@images/a17d006e-5c0d-4004-9fe7-650c7fabd690.png"
+            src="./assets/circle-linkedin-512.webp"
             className="linkContact"
           />
         </a>
